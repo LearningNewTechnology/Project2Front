20a45cc3adc4c1576c308da93183fa25a03f3d1d
jtran64 <jtran416069@gmail.com>
2019-04-28 19:22:53 -0500

    Styling

--- src/app/post.ts
+++ src/app/post.ts
@@ -1,10 +1,11 @@
 import { Like } from './like';
+import { User } from './user';
 
 export class Post{
     id: number;
     description: string;
     postedDate: string;
-    authorId: number;
+    author: User;
     picture: string;
     likedBy: Like[];
 }
